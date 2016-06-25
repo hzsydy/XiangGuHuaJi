26ea867ae28f6fa44526800ccda9e0f82aa0f8b5
Du <hzsydy@foxmail.com>
2016-06-25 23:42:12 +0800

    init

---
+++ XiangGuHuaJi/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>  
+#include <opencv2/core/core.hpp> 
+#include <opencv2/highgui/highgui.hpp>   
+using namespace cv;  
+using std::cout;
+using std::endl;
+
+int main() 
+{
+
+} 
