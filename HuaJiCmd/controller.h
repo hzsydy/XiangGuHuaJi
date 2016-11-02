a07588d13e4dd8512dba3e68c7bbda6c4152a2d1
Du <hzsydy@foxmail.com>
2016-11-02 21:53:52 +0800

    分离了controller，Game成为类库

---
+++ HuaJiCmd/controller.h
@@ -0,0 +1,41 @@
+/* XiangGuHuaJi 2016, controller.h
+ *
+ */
+
+#ifndef _XIANGGUHUAJI_CONTROLLER_H__
+#define _XIANGGUHUAJI_CONTROLLER_H__
+
+#include <vector>
+#include <iostream>
+#include "definition.h"
+#include "game.h"
+#include "player.h"
+#include <tuple>
+#include <algorithm>
+
+using std::vector;
+
+
+namespace XGHJ
+{
+	class Controller 
+	{
+	public:
+		Controller(Game& g, vector<Player>& p) 
+			: game_(g), players_(p) 
+		{
+			playerSize_ = game_.getPlayerSize();
+		}
+
+		void run();
+		inline bool isValid(){return isValid_;}
+
+	protected:
+		Game& game_;
+		vector<Player>& players_;
+		bool isValid_;
+		TId playerSize_;
+	};
+}
+
+#endif
